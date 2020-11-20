066fe1a481de5184b958ed9f6e907718dafa8915
Andres Parada <drelo@users.noreply.github.com>
2020-11-20 13:48:43 -0300

    Create selectbestfuseparts.sh
    To run this put all .fas files in a folder called 'FASTA'

---
+++ selectbestfuseparts.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for i in FASTA/*.fas ; do iqtree -s $i; done 
+./AMAS/amas/AMAS.py concat -i ./FASTA/*fas -f fasta -d dna -t concatenated.fas -p partition.part
+grep 'Best-fit model' ./FASTA/*.iqtree > MODEL
+awk '{print $1 $NF}' MODEL  > MODEL2   
+sed -i 's~.fas~~g; s~.iqtree:Best-fit~ ~g; s~$~,~g' MODEL2
+awk '{print $2" "$1}' MODEL2 > MODEL3
+awk '{print $2" "$NF}' partition.part > LENGTH
+paste MODEL3 LENGTH > partitionmodels
+iqtree -s concatenated.fas -p partitionmodels
