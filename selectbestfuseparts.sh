9b87e23fe6bf026f0524e2fd777b09b08b11385f
Andres Parada <drelo@users.noreply.github.com>
2020-11-20 20:42:02 -0300

    Update selectbestfuseparts.sh

--- selectbestfuseparts.sh
+++ selectbestfuseparts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in FASTA/*.fas ; do iqtree -s $i; done 
+for i in FASTA/*.fas ; do ./iqtree -s $i; done 
 ./AMAS/amas/AMAS.py concat -i ./FASTA/*fas -f fasta -d dna -t concatenated.fas -p partition.part
 grep 'Best-fit model' ./FASTA/*.iqtree > MODEL
 awk '{print $1 $NF}' MODEL  > MODEL2   
@@ -8,4 +8,4 @@ sed -i 's~./FASTA/~~g; s~.fas~~g; s~.iqtree:Best-fit~ ~g; s~$~,~g' MODEL2
 awk '{print $2" "$1}' MODEL2 > MODEL3
 awk '{print $2" "$NF}' partition.part > LENGTH
 paste MODEL3 LENGTH > partitionmodels
-iqtree -s concatenated.fas -p partitionmodels
+./iqtree -s concatenated.fas -p partitionmodels
